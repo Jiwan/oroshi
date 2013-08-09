dcb7f68ae4dc63915eb9d8831b2cf3e8d070f379
Jiwan <jguegant@gmail.com>
2013-08-09 16:13:39 +0200

    Fixed a namespace error

--- login/login-event.hpp
+++ login/login-event.hpp
@@ -8,36 +8,41 @@
 #include <common/network/network-event.hpp>
 
 // Note: I am not sure if a forward declaration of LoginPacketHandler is necessary (a good old include could be enough).
+#include "login-packet-handler.hpp"
 
-
-typedef oroshi::common::network::NetworkEvent<LoginPacketHandler, oroshi::common::network::BasicPacketCrypt> LoginNetworkEvent;
-typedef oroshi::common::network::NetworkClient<LoginPacketHandler, oroshi::common::network::BasicPacketCrypt> LoginNetworkClient;
-
-
-class UserLoginEvent: LoginNetworkEvent
+namespace oroshi
 {
-public:
-    UserLoginEvent(std::shared_ptr<LoginNetworkClient> client, std::string& account, std::string& password):
-        LoginNetworkEvent(client),
-        account_(account),
-        password_(password)
+    namespace login
     {
+        typedef oroshi::common::network::NetworkEvent<LoginPacketHandler, oroshi::common::network::BasicPacketCrypt> LoginNetworkEvent;
+        typedef oroshi::common::network::NetworkClient<LoginPacketHandler, oroshi::common::network::BasicPacketCrypt> LoginNetworkClient;
 
+        class UserLoginEvent: LoginNetworkEvent
+        {
+        public:
+            UserLoginEvent(std::shared_ptr<LoginNetworkClient> client, std::string& account, std::string& password):
+                LoginNetworkEvent(client),
+                account_(account),
+                password_(password)
+            {
+
+            }
+
+            const std::string& account() const
+            {
+                return account_;
+            }
+
+            const std::string& password() const
+            {
+                return password_;
+            }
+
+        private:
+            std::string account_;
+            std::string password_;
+        };
     }
-
-    const std::string& account() const
-    {
-        return account_;
-    }
-
-    const std::string& password() const
-    {
-        return password_;
-    }
-
-private:
-    std::string account_;
-    std::string password_;
-};
+}
 
 #endif // LOGINEVENT_HPP
